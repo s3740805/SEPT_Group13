a70cab5d334ec853c5af406d32cf8f5420d78b25
s3747380 <s3747380@rmit.edu.vn>
2020-07-23 16:44:18 +0700

    "date and time in java.sql"

--- src/main/java/model/Booking.java
+++ src/main/java/model/Booking.java
@@ -1,6 +1,8 @@
 package model;
 
 import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Time;
 
 @Entity
 @Table(name="booking")
@@ -12,8 +14,11 @@ public class Booking {
     private Patient patient;
     @ManyToOne(cascade = CascadeType.ALL)
     private Doctor doctor;
-    @ManyToOne(cascade = CascadeType.ALL)
     private Time time;
+    private Date date;
+//  @ManyToOne(cascade = CascadeType.ALL)
+//  private Time time;
+
 
 
     public int getId() {
@@ -22,14 +27,6 @@ public class Booking {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public Time getTime() {
-        return time;
-    }
-
-    public void setTime(Time time) {
-        this.time = time;
     }
 
     public Doctor getDoctor() {
@@ -46,6 +43,22 @@ public class Booking {
 
     public void setPatient(Patient patient) {
         this.patient = patient;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 
     public Booking() {
--- src/main/java/model/Time.java
+++ src/main/java/model/Time.java
@@ -1,44 +1,44 @@
-package model;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name="time")
-public class Time {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    @Column
-    private String date;
-    @Column
-    private String time;
-
-
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public Time() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-}
+//package model;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name="time")
+//public class Time {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private int id;
+//    @Column
+//    private String date;
+//    @Column
+//    private String time;
+//
+//
+//
+//    public String getTime() {
+//        return time;
+//    }
+//
+//    public void setTime(String time) {
+//        this.time = time;
+//    }
+//
+//    public Time() {
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(String date) {
+//        this.date = date;
+//    }
+//}
