7897cf44c7a1e99f1a307235f0d829daea9c789c
Linh Nguyen <45310444+s3740805@users.noreply.github.com>
2020-08-25 21:32:36 +0700

    Update WebApplicationTest.java

--- Frontend+Register+Login/src/test/java/com/WebApplicationTest.java
+++ Frontend+Register+Login/src/test/java/com/WebApplicationTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebApplicationTest {
+public class WebApplicationTest {
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 }
